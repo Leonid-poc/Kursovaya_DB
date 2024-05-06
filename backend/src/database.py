23cf01564213722cc22ac972e9b905c9f620860b
Leonid-poc <legeorg2004@gmail.com>
2024-05-06 21:07:24 +0500

    init database session connect

---
+++ backend/src/database.py
@@ -0,0 +1,19 @@
+import databases
+
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import DeclarativeBase, sessionmaker
+
+DATABASE_URL = "postgresql+asyncpg://postgres:1234@localhost/kursovaya_db"
+
+database = databases.Database(DATABASE_URL)
+Base: DeclarativeBase = declarative_base()
+engine = create_async_engine(DATABASE_URL)
+async_session = sessionmaker(
+    engine, class_=AsyncSession,
+    expire_on_commit=False
+)
+
+async def get_session():
+    async with async_session() as session:
+        yield session
