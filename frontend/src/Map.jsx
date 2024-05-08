f870db180f51429809df6efc6b36d68cb5ee74c7
Leonid-poc <legeorg2004@gmail.com>
2024-05-08 11:45:05 +0500

    edit logic

--- frontend/src/Map.jsx
+++ frontend/src/Map.jsx
@@ -1,25 +1,25 @@
-import { MapContainer, TileLayer, useMap, Marker, Popup, Polyline } from "react-leaflet";
-import React, { useEffect } from 'react'
-import "leaflet/dist/leaflet.css"
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import Text2html from "./vendor/Text2html";
+import "leaflet/dist/leaflet.css";
 
-const Map = ({mode}) => {
-	useEffect(() => {
-		console.log(mode);
-	}, [mode]);
-  	const center = [51.77629, 55.110047];
-  	return (
-      <MapContainer center={center} zoom={16.77} scrollWheelZoom={true}>
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <Marker position={center}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
-      </MapContainer>
-    );
-}
+// eslint-disable-next-line react/prop-types
+const Map = ({ mode }) => {
+  const e = <Text2html mode={mode}/>;
+  const center = [51.77629, 55.110047];
+  return (
+    <MapContainer center={center} zoom={16.77} scrollWheelZoom={false}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={center}>
+        <Popup>
+          Test
+        </Popup>
+      </Marker>
+      {e}
+    </MapContainer>
+  );
+};
 
-export default Map
+export default Map;
--- frontend/src/Panel.jsx
+++ frontend/src/Panel.jsx
@@ -1,24 +1,61 @@
-import React, {useState} from 'react'
+import {useState} from 'react'
 
+// eslint-disable-next-line react/prop-types
 const Panel = ({setMode}) => {
     const [isEditPolyline, setIsEditPolyline] = useState(false);
+    const [isAddingBS, setIsAddingBS] = useState(false);
+    const [isDelConn, setIsDelConn] = useState(false);
+
+    const handleDel = () => {
+        if (!isDelConn) setMode("DelConn");
+        else setMode("CancelDelConn");
+        setIsAddingBS(false);
+        setIsEditPolyline(false);
+        setIsDelConn(!isDelConn);
+    }
+
+    const handleAdd = () => {
+        if (!isAddingBS) setMode("AddBS");
+        else setMode("CancelBS");
+        setIsAddingBS(!isAddingBS);
+        setIsEditPolyline(false);
+        setIsDelConn(false);
+    }
 
     const handleEdit = () => {
         if (isEditPolyline) setMode("CancelEdit");
         else setMode("Edit");
+        setIsAddingBS(false);
         setIsEditPolyline(!isEditPolyline);
+        setIsDelConn(false);
     }
   return (
     <div className="buttons">
-        <input type="button" value="Добавить остановку" className="element-map" onClick={() => {setMode("AddBS")}}/>
+        {!isAddingBS ?
+        (<input type="button" value="Добавить остановку" className={"element-map " + (isAddingBS && "hideButton")} onClick={handleAdd}/>) :
+        (<div className='AddBS settings'>
+            <input type="button" value="Подтвердить" className='element-map' onClick={() => {setMode("ApplyBS")}}/>
+            <input type="button" value="Удалить выбранное" className='element-map' onClick={() => {setMode("DelBS")}}/>
+            <input type="button" value="Отменить" className='element-map' onClick={handleAdd}/>
+        </div>)
+        }
+        
         {!isEditPolyline ? 
             (<input type="button" value="Соеденить остановки" className="element-map" onClick={handleEdit}/>) :
-            (<div className='EditPoligonLines'>
+            (<div className='EditPoligonLines settings'>
                 <input type="button" value="Сделать следующее соединение" onClick={() => {setMode("NextEdit")}}/>
-                <input type="button" value="Применить" onClick={() => {setMode("Apply")}}/>
+                <input type="button" value="Применить" onClick={() => {setMode("ApplyEdit")}}/>
                 <input type="button" value="Отменить" onClick={handleEdit}/>
-            </div>)}
-        <input type="button" value="Удалить соединения" className="element-map" onClick={() => {setMode("DeleteConn")}}/>
+            </div>)
+        }
+
+        {!isDelConn ?
+            (<input type="button" value="Удалить соединения" className="element-map" onClick={handleDel}/>) :
+            (<div className='DelConn settings'>
+                <input type="button" value="Подтвердить" className='element-map' onClick={() => {setMode("ApplyDel")}}/>
+                <input type="button" value="Отменить" className='element-map' onClick={handleDel}/>
+            </div>)
+        }
     </div>
   )
 }
